bc0cf9c93d59f9bdec90b0f61550e180434050cc
Fernando Simões <f.simoes047@gmail.com>
2024-02-21 20:51:29 +0000

    versão inicial05

--- hw.py
+++ hw.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app=Flask(__name__)
 
@@ -7,13 +7,39 @@ def hello_world():
     return "<h1>Hello, World!</h1>\
             <p> this is a test server</p>"
 
+@app.route("/home", methods=['GET','POST'], defaults={"name":"Nobody"})
+@app.route("/home/<name>", methods=['GET','POST'])
+def name(name):
+    return f"""Hello {name}!"""
 
-@app.route("/name/")
-def name():
-    return """<form>\
-            <label for="fname">First name:</label><br>\
-            <input type="text" id="fname" name="fname"><br>\
-            <label for="lname">Last name:</label><br>\
-            <input type="text" id="lname" name="lname">\
-            <input type="submit" value="Submit answer">
+@app.route('/query')
+def query():
+    fname = request.args.get('name')
+    location = request.args.get('location')
+    return f"Hello {fname}, you are in {location}. This is the query page"
+
+@app.route('/theform')
+def theform():
+    return """<form method='POST' action='/process'>
+                <input type='text' name='name'>
+                <input type='text' name='location'>
+                <input type='submit' value='Submit'>
             </form>"""
+
+@app.route('/process', methods=['POST'])
+def process():
+    name=request.form['name']
+    location=request.form['location']
+    return f"Hello {name}, you are from {location}"
+
+@app.route('/processjson', methods=['POST'])
+def processjson():
+    data = request.get_json()
+    name=data['name']
+    location=data['location']
+    randomlist=data['randomlist']
+
+    return {"result":"sucess!","name": name, "location": location, "randomkey": randomlist[1]}
+
+if __name__ == '__main__':
+    app.run(debug=True)
