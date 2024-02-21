a7edc53de3cf14a541a7de28babfb309df6b8df0
Fernando Simões <f.simoes047@gmail.com>
2024-02-21 18:35:03 +0000

    versão inicial04

--- hw.py
+++ hw.py
@@ -4,5 +4,16 @@ app=Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<h1>Hello, World!</h1>\
+            <p> this is a test server</p>"
 
+
+@app.route("/name/")
+def name():
+    return """<form>\
+            <label for="fname">First name:</label><br>\
+            <input type="text" id="fname" name="fname"><br>\
+            <label for="lname">Last name:</label><br>\
+            <input type="text" id="lname" name="lname">\
+            <input type="submit" value="Submit answer">
+            </form>"""
